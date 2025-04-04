75d96b0e4cabff767b86429eaa55f4d0a5eeb70e
ameer ahmed <136123824+am17jx@users.noreply.github.com>
2025-04-04 23:27:05 +0300

    Update MainIPV4.kt

--- Thechance-main/ipv4/MainIPV4.kt
+++ Thechance-main/ipv4/MainIPV4.kt
@@ -3,14 +3,13 @@ fun ip(ip: String): Boolean {
     if (segments.size != 4) return false
 
     for (segment in segments) {
-        if (segment.isEmpty()  segment.length > 3  
-            (segment.length > 1 && segment.startsWith("0"))) {
+        if (segment.isEmpty() || (segment.length > 1 && segment.startsWith("0"))) {
             return false
         }
 
         val num = segment.toIntOrNull() ?: return false
         if (num !in 0..255) return false
     }
-    
+
     return true
 }
